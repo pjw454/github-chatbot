a41d9bef36dd7e79d78f653fa176548c787bfb9a
jung Park <pyj8837@naver.com>
2019-09-04 17:45:18 +0900

    update app.py

--- app.py
+++ app.py
@@ -43,7 +43,9 @@ def index():
 
 if __name__ == '__main__':
     print(__name__)
-    app.run(debug=True)
+    import os
+    port = int(os.getenv('PORT', 5000))
+    app.run(host = '0.0.0.0', port=port ,debug=True)
 
 
 #https://api.telegram.org/bot956873054:AAF6v0VfOcmfX-B_Knkszy5UyjNIGlxtyNo/setWebhook?url=https://bfa0ee75.ngrok.io/956873054:AAF6v0VfOcmfX-B_Knkszy5UyjNIGlxtyNo
